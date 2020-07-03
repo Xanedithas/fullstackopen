b3b09e155047b3283e10c5c2c1571978344600de
Xanedithas <mikestaadegaard@hotmail.com>
2020-07-03 17:35:14 +0200

    Exercises 3.1.-3.9.

--- part2/phonebook/src/services.js
+++ part2/phonebook/src/services.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "http://localhost:3001/api/persons"
 
 const read = async () => {
     const request = axios.get(baseUrl)
